a6756626094ee565386e3ebcadf24d8cf3ede6e1
ArberBajraktari <arberbajraktari02@gmail.com>
2022-03-01 20:51:45 +0100

    In Class: Data Binding done

--- src/main/java/com/example/tourplanner/StringConcatenationController.java
+++ src/main/java/com/example/tourplanner/StringConcatenationController.java
@@ -5,22 +5,30 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+
 public class StringConcatenationController {
 
+    private StringConcatenationModel model = new StringConcatenationModel();
+
+
     @FXML
-    public TextField LeftStringTextField;
+    public void initialize() {
+        this.leftStringTextField.textProperty().bindBidirectional(model.leftValueProperty());
+        this.rightStringTextField.textProperty().bindBidirectional(model.rightValueProperty());
+        this.stringConcatenationResultLabel.textProperty().bind(model.resultProperty());
+
+    }
+
     @FXML
-    public Label StringConcatenationResultLabel;
+    public TextField leftStringTextField;
     @FXML
-    public TextField RightStringTextField;
+    public Label stringConcatenationResultLabel;
+    @FXML
+    public TextField rightStringTextField;
+
 
     @FXML
     public void concatenateStrings(MouseEvent mouseEvent) {
-        String LeftString = LeftStringTextField.getText();
-        String RightString = RightStringTextField.getText();
-        String ResultString = LeftString + " " + RightString;
-        StringConcatenationResultLabel.setText(ResultString);
-        System.out.println(ResultString);
-
+        this.model.concat();
     }
 }
---
+++ src/main/java/com/example/tourplanner/StringConcatenationModel.java
@@ -0,0 +1,41 @@
+package com.example.tourplanner;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StringConcatenationModel {
+    private StringProperty leftValue = new SimpleStringProperty();
+    private StringProperty rightValue = new SimpleStringProperty();
+    private StringProperty result = new SimpleStringProperty();
+
+    public void concat(){
+        System.out.println(this.getLeftValue() + this.getRightValue());
+        this.result.setValue(this.getLeftValue() + this.getRightValue());
+    }
+
+    public String getLeftValue() {
+        return leftValue.get();
+    }
+
+    public StringProperty leftValueProperty() {
+        return leftValue;
+    }
+
+    public String getRightValue() {
+        return rightValue.get();
+    }
+
+    public StringProperty rightValueProperty() {
+        return rightValue;
+    }
+
+    public String getResult() {
+        return result.get();
+    }
+
+    public StringProperty resultProperty() {
+        return result;
+    }
+
+
+}

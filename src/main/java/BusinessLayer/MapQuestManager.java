3bcd617d74f0988a113db155abe741735eee2f54
ArberBajraktari <arberbajraktari02@gmail.com>
2022-05-27 17:43:30 +0200

    MapQuestManager fixed

--- src/main/java/BusinessLayer/MapQuestManager.java
+++ src/main/java/BusinessLayer/MapQuestManager.java
@@ -2,9 +2,7 @@ package BusinessLayer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
